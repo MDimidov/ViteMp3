50c5907e05b7b12637aa496f4f6340e833362be7
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-19 14:48:37 +0200

    Practice 3D Transform APIs, Gradient APIs rounded/radial

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,31 @@ function App() {
         </div>
       </div> */}
 
-      <div className='@container border p-4'>
+      {/* <div className='@container border p-4'>
         <div className='flex gap-4'>
           <div className='bg-blue-500 h-40 w-64 '></div>
           <div className='bg-green-500 h-40 w-64 @min-md:@max-xl:hidden'></div>
           <div className='bg-red-500 h-40 w-64 '></div>
-          {/* <div className='bg-yellow-500 h-40 w-64'></div> */}
+          <div className='bg-yellow-500 h-40 w-64'></div>
         </div>
-      </div>
+      </div> */}
 
+      {/* <div className='min-h-screen flex items-center justify-center bg-gray-100 perspective-1000'>
+        <div className='bg-blue-500 h-96 w-96 flex items-center justify-center text-white text-4xl font-bold transform hover:scale-120 hover:translate-z-20 translation-transform duration-300'>3D rotate
+        </div>
+      </div> */}
+
+
+      {/* <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+        <div className='bg-linear-45 from-blue-500 to-green-500 p-8 rounded-3xl text-2xl font-bold text-white hover:scale-110'>
+          Angled Gradient APIs
+        </div>
+      </div> */}
+
+      <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+        <div className='size-64 rounded-full bg-conic-180 from-indigo-600 via-indigo-50 to-indigo-600'></div>
+        <div className='bg-radial from-yellow-500 to-red-500 h-64 w-64 rounded-full ml-5'></div>
+      </div>
     </>
   )
 }

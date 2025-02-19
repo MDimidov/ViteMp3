fea6ce1e48b92d6cd792cdee91b56db282774a5a
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-19 13:54:28 +0200

    Practice tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className='bg-deepblue p-12'>
+        <h1 className='text-5xl font-bold text-white'>Welcome with tailwind</h1>
 
-      <h1 class="text-3xl text-green-300 font-bold underline">
-        Hello world!
-      </h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className='bg-amber text-alambaster text-2xl font-bold mt-12 rounded px-6 py-4'>Click me!</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }

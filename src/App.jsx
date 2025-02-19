8cc85483ea46be11d3b7c280d0566323d248253c
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-19 14:08:37 +0200

    practice utilities, placeholder, plugins

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ function App() {
 
   return (
     <>
-      <div className='bg-deepblue p-12'>
+      <div className='bg-deepblue container'>
         <h1 className='text-5xl font-bold text-white'>Welcome with tailwind</h1>
 
-        <button className='bg-amber text-alambaster text-2xl font-bold mt-12 rounded px-6 py-4'>Click me!</button>
+        <button className='bg-amber text-alambaster border-4 text-2xl font-bold mt-12 rounded px-6 py-4 animate-pulse'>Click me!</button>
+
+        <input type='text' placeholder='Enter your name' className='bg-white text-lg text-amber border-2 p-4 rounded-2xl ml-4 outline-0'/>
       </div>
 
 

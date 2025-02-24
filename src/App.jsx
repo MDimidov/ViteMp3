12e09706d8ff1b7b2a45e5184924afd89e179a78
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-24 18:55:39 +0200

    Add error page, register form

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import { DarkMode } from "./components/Practice/DarkMode";
 import Hero from "./components/Hero/Hero";
+import Register from "./components/Register/Register";
+import Error404 from "./components/Errors/error404";
 
 
 export default function App() {
   return (
     <div className="min-w-[850px]">
       <Header />
-      <Login />
+      {/* <Login /> */}
+      {/* <Error404 /> */}
+      <Register />
       {/* <DarkMode /> */}
-      <Hero />
+      {/* <Hero /> */}
       <FooterMusic />
     </div>
   );
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,84 @@
+export default function Register() {
+    return (
+      <>
+        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              alt="Your Company"
+              src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+              className="mx-auto h-10 w-auto"
+            />
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">
+              Sign in to your account
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+              <div>
+                <label htmlFor="email" className="block text-sm/6 font-medium">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    autoComplete="email"
+                    className="block w-full p-2 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm/6 font-medium">
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full p-2 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password-confirmation" className="block text-sm/6 font-medium">
+                    Password confirmation
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password-confirmation"
+                    name="password-confirmation"
+                    type="password"
+                    required
+                    className="block w-full p-2 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </>
+    )
+  }
+  

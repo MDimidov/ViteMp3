33ffd9f94543e49b7770eb35ebf9bcdb56c57c02
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-06 13:59:53 +0200

    Update Discover & DiscoverItem

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -2,7 +2,9 @@ import DiscoverItem from "./DiscoverItem";
 
 export default function Discover() {
     return (
-        <section className="flex justify-between gap-4 animate-fade-in mx-auto my-20 overflow-auto">
+        <section className="flex justify-between gap-4 animate-fade-in mx-auto my-20 overflow-auto scrollable-container ">
+            <DiscoverItem />
+            <DiscoverItem />
             <DiscoverItem />
             <DiscoverItem />
             <DiscoverItem />
--- src/components/Discover/DiscoverItem.jsx
+++ src/components/Discover/DiscoverItem.jsx
@@ -1,26 +1,26 @@
 import { EllipsisIcon, HeartIcon, PlayIcon } from "lucide-react";
 
 export default function DiscoverItem() {
-
+    const btnClassCircle = 'hover:bg-black/15 dark:hover:bg-white/10 cursor-pointer w-10 h-10 rounded-full active:scale-90 hover:scale-110 text-gray-300';
     return (
         <div>
-            <div className="h-50 w-50 rounded-2xl overflow-hidden group transform transition">
+            <div className="h-50 w-50 rounded-2xl overflow-hidden group transform transition ">
                 <img
                     src="https://t3.ftcdn.net/jpg/12/85/10/50/1000_F_1285105074_WokvIntOeNb395MfdRWEgkklpNFFn3b3.jpg"
                     alt="test"
-                    className="w-full h-full relative object-cover group-hover:brightness-50 duration-300"
+                    className="w-full h-full relative object-cover group-hover:brightness-80 dark:group-hover:brightness-50 duration-300"
                 />
 
                 <div className="-top-14 px-2 relative flex items-center justify-between w-full">
-                    <button className="hover:bg-black dark:hover:bg-white/10 cursor-pointer w-10 h-10 rounded-full active:scale-90">
-                        <PlayIcon className="m-auto" />
+                    <button className={btnClassCircle}>
+                        <PlayIcon className="m-auto" fill="currentColor"/>
                     </button>
 
                     <div className="hidden items-center justify-between gap-2 r-0 group-hover:flex transform transition duration-1000">
-                        <button className="hover:bg-black dark:hover:bg-white/10 cursor-pointer w-10 h-10 rounded-full active:scale-90">
+                        <button className={btnClassCircle}>
                             <EllipsisIcon className="m-auto" />
                         </button>
-                        <button className="hover:bg-black dark:hover:bg-white/10 cursor-pointer w-10 h-10 rounded-full active:scale-90">
+                        <button className={`${btnClassCircle} hover:text-red-500 transition-colors`}>
                             <HeartIcon className="m-auto" />
                         </button>
                     </div>

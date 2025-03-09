3c86ec25a4aa16e2cd00c5e129e92d320a76e4ad
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-03-09 14:43:46 +0200

    Progress to scroll arrows

--- src/components/Discover/PictureSongs.jsx
+++ src/components/Discover/PictureSongs.jsx
@@ -4,24 +4,25 @@ import PictureSongItem from "./PictureSongItem";
 import { useState } from "react";
 
 export default function PictureSongs() {
-    // let [current, setCurrent] = useState(0);
+    let [current, setCurrent] = useState(0);
 
     let previousSlide = () => {
-        // if (current === 0) setCurrent(slides.length - 1);
-        // else setCurrent(current - 1);
+        if (current === 0) setCurrent(slides.length - 1);
+        else setCurrent(current - 1);
     };
 
     let nextSlide = () => {
-        // if (current === slides.length - 1) setCurrent(0);
-        // else setCurrent(current + 1);
+        if (current === slides.length - 1) setCurrent(0);
+        else setCurrent(current + 1);
     };
 
 
     return (
         <>
 
-            <Section className="my-20 max-w-full">
+            <Section className="my-20 max-w-full group">
                 <p className="text-2xl hover:underline cursor-pointer">New Music</p>
+
                 <section className="flex justify-between gap-4 animate-fade-in mx-auto overflow-auto scrollable-container mt-2 bg-scroll snap-x">
                     <PictureSongItem className="snap-center" />
                     <PictureSongItem className="snap-center" />
@@ -29,15 +30,16 @@ export default function PictureSongs() {
                     <PictureSongItem className="snap-center" />
                     <PictureSongItem className="snap-center" />
                     <PictureSongItem className="snap-center" />
-                    <div className="-top-1/2 h-full w-1/2 justify-between items-center flex gap-10 text-red-500 px-10 text-3xl absolute size-20 z-10">
-                        <button>
-                            <ChevronLeftIcon />
-                        </button>
-                        <button>
-                            <ChevronRightIcon />
-                        </button>
-                    </div>
                 </section>
+                <div className="-translate-y-40 h-full w-full m-auto justify-between items-center flex text-red-500 px-8 text-3xl size-20 z-990">
+                    <button className="bg-white">
+                        <ChevronLeftIcon />
+                    </button>
+                    <button className="bg-white">
+                        <ChevronRightIcon />
+                    </button>
+                </div>
+
             </Section>
 
         </>
